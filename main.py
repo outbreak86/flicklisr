e1f0fb5ffc1ff8ef8a774d090fc631f2cb613eb0
Royce R <royce.rocket86@gmail.com>
2017-01-24 15:44:17 -0600

    Flick 2 done, added bonus CSS

--- main.py
+++ main.py
@@ -1,34 +1,110 @@
 import webapp2
-import random
-
-def getRandomMovie():
-
-	# list of movies
-	movies = [
-		"Sweeney Todd",
-		"Toy Story",
-		"The Matrix",
-		"Ghost Busters",
-		"Saw"
-	]
-
-	index = random.randint(0,len(movies)-1)
-
-	return movies[index]
 
 
-class MainHandler(webapp2.RequestHandler):
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <link type="text/css" rel="stylesheet" href="serveup/mystyle.css">
+    <title>FlickList</title>
+</head>
+<body>
+    <h1>FlickList</h1>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+
+class Index(webapp2.RequestHandler):
+    """ Handles requests coming in to '/' (the root of our site)
+        e.g. www.flicklist.com/
+    """
+
     def get(self):
-    	movieHeader = "<h1>Movie of the day!</h1>"
-    	movieparagraph = "<p>"+getRandomMovie()+"</p>"
-    	tomorrowHead = "<h1>Tomorrow's movie of the day!</h1>"
-    	tomorrowparagraph = "<p>"+getRandomMovie()+"</p>"
 
-    	while movieparagraph == tomorrowparagraph:
-    		tomorrowparagraph = "<p>"+getRandomMovie()+"</p>"
-    	
-        self.response.write(movieHeader+movieparagraph+tomorrowHead+tomorrowparagraph)
+        edit_header = "<h3>Edit My Watchlist</h3>"
 
+        # a form for adding new movies
+        add_form = """
+        <form action="/add" method="post">
+            <label>
+                I want to add
+                <input type="text" name="new-movie"/>
+                to my watchlist.
+            </label>
+            <input type="submit" value="Add It"/>
+        </form>
+        """
+
+        # TODO 1
+        # Include another form so the user can "cross off" a movie from their list.
+        remove_form = """
+        <form action="/remove" method="post">
+            <label>
+                I want to remove
+                <input type="text" name="old-movie"/>
+                from my watchlist.
+            </label>
+            <input type="submit" value="Remove It"/>
+        </form>
+        """
+
+        # TODO 4 (Extra Credit)
+        # modify your form to use a dropdown (<select>) instead a
+        # text box (<input type="text"/>)
+
+
+        content = page_header + edit_header + add_form + remove_form + page_footer
+        self.response.write(content)
+
+
+class AddMovie(webapp2.RequestHandler):
+    """ Handles requests coming in to '/add'
+        e.g. www.flicklist.com/add
+    """
+
+    def post(self):
+        # look inside the request to figure out what the user typed
+        new_movie = self.request.get("new-movie")
+
+        # build response content
+        new_movie_element = "<strong>" + new_movie + "</strong>"
+        sentence = new_movie_element + " has been added to your Watchlist!"
+
+        content = page_header + "<p>" + sentence + "</p>" + page_footer
+        self.response.write(content)
+
+
+# TODO 2
+# Create a new RequestHandler class called CrossOffMovie, to receive and
+# handle the request from your 'cross-off' form. The user should see a message like:
+# "Star Wars has been crossed off your watchlist".
+class CrossOffMovie(webapp2.RequestHandler):
+    """ Handles requests coming in to '/remove'
+        e.g. www.flicklist.com/remove
+    """
+
+    def post(self):
+        # look inside the request to figure out what the user typed
+        old_movie = self.request.get("old-movie")
+
+        # build response content
+        old_movie_element = "<strike>" + old_movie + "</strike>"
+        sentence = old_movie_element + " has been removed from your Watchlist!"
+
+        content = page_header + "<p>" + sentence + "</p>" + page_footer
+        self.response.write(content)
+
+
+
+# TODO 3
+# Include a route for your cross-off handler, by adding another tuple to the list below.
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index),
+    ('/add', AddMovie),
+    ('/remove',CrossOffMovie)
 ], debug=True)
